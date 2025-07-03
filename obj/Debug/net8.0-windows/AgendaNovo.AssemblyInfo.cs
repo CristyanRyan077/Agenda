f77b75b9338186eb31433249c035d8404379f5e2
cristyan <crisryan077@gmail.com>
2025-07-03 19:55:51 -0300

    teste

--- obj/Debug/net8.0-windows/AgendaNovo.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/AgendaNovo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgendaNovo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9d3e9293851a86b7c75c18d55eb00e789968376")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f6a3998f5db61d592f6a9143fdb40b38a73148b")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgendaNovo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgendaNovo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
