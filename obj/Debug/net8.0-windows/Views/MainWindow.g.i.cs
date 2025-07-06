39255d0854c17f0bf82b3a317eca3dcf487109e5
cristyan <crisryan077@gmail.com>
2025-07-06 16:43:07 -0300

    copiado agenda 2 para agenda 1

--- obj/Debug/net8.0-windows/Views/MainWindow.g.i.cs
+++ obj/Debug/net8.0-windows/Views/MainWindow.g.i.cs
@@ -153,7 +153,7 @@ namespace AgendaNovo {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/AgendaNovo;component/views/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/AgendaNovo;V1.0.0.0;component/views/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
