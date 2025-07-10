0d3a14f8c2b6fa32d7d4346e41060a83b7c6ed93
ALUNO <ALUNO@2857AL18>
2025-07-10 14:26:19 -0300

    Update 1.1.2
    -trocado a senha do app publico para localhost

--- obj/Debug/net8.0-windows/Views/GerenciarClientes.g.i.cs
+++ obj/Debug/net8.0-windows/Views/GerenciarClientes.g.i.cs
@@ -135,7 +135,7 @@ namespace AgendaNovo {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/AgendaNovo;V1.0.0.0;component/views/gerenciarclientes.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/AgendaNovo;component/views/gerenciarclientes.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\GerenciarClientes.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
