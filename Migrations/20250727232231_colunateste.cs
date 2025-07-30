ffae96a023007cda82956303cae3496fbdc9e9e8
cristyan <crisryan077@gmail.com>
2025-07-29 22:33:10 -0300

    subido migrations

---
+++ Migrations/20250727232231_colunateste.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AgendaNovo.Migrations
+{
+    /// <inheritdoc />
+    public partial class colunateste : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "teste",
+                table: "Agendamentos",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "teste",
+                table: "Agendamentos");
+        }
+    }
+}
